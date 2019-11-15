ddfe791022ee0b0905c29a16ec7e49b666d0d030
gstirlingAC <gordon.stirling@ayrshire.ac.uk>
2019-11-15 10:42:28 +0000

    Hex Cell and Hex Grid
    Added Hex Cell and Grid components to generate square grid

---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+    public int width = 6;
+    public int height = 6;
+
+    public HexCell cellPrefab;
+
+    HexCell[] cells;
+
+    void Awake()
+    {
+        cells = new HexCell[height * width];
+
+        for (int z = 0, i = 0; z < height; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                CreateCell(x, z, i++);
+            }
+        }
+    }
+
+    void CreateCell(int x, int z, int i)
+    {
+        Vector3 pos;
+        pos.x = x * 10f;
+        pos.y = 0f;
+        pos.z = z * 10f;
+
+        HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = pos;
+    }
+}
